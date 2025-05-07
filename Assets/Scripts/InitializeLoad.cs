7260d4c9ae597c31dc14f2544fa65e421a11743c
zhouxuanyu536 <74370651+zhouxuanyu536@users.noreply.github.com>
2025-05-07 11:00:00 +0800

    Add files via upload

---
+++ Assets/Scripts/InitializeLoad.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InitializeLoad : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoomScene/PlayerDetail.cs
@@ -0,0 +1,65 @@
+﻿using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDetail : NetworkBehaviour
+{
+    [SerializeField] private Button Kickout;
+    [SerializeField] private TextMeshProUGUI PlayerName;
+    [SerializeField] private Image isReady;
+    [SerializeField] private Sprite isReadyImage;
+    [SerializeField] private Sprite isNotReadySprite;
+    
+    private ulong clientId;
+    private string Name;
+    private bool ready;
+    private int num;
+
+    private void Start()
+    {
+        Kickout.onClick.AddListener(KickoutPlayer);
+    }
+    private void Update()
+    {
+        PlayerName.text = Name;
+        if (ready)
+        {
+            isReady.sprite = isReadyImage;
+        }
+        else
+        {
+            isReady.sprite = isNotReadySprite;
+        }
+    }
+    public void SetClientId(ulong clientId)
+    {
+        this.clientId = clientId;
+    }
+    public void SetPlayerName(string name)
+    {
+        this.Name = name;
+    }
+    public void SetPlayerReady(bool ready)
+    {
+        this.ready = ready;
+    }
+    public void SetNum(int num)
+    {
+        this.num = num;
+    }
+
+    public void KickoutPlayer()
+    {
+        //    if(NetworkManager.Singleton.IsServer)
+        //    {
+        //        ulong id = GameMultiplayer.Instance.playerDataNetworkList[num].clientId;
+        //        GameMultiplayer.Instance.OnPlayerQuit(id);
+        //    }
+    }
+
+    public override void OnDestroy()
+    {
+        Kickout.onClick.RemoveListener(KickoutPlayer);
+    }
+}
