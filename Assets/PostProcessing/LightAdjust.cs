bb37cce87768607d968f6f9d5670a5f9ec8a9433
zhouxuanyu536 <74370651+zhouxuanyu536@users.noreply.github.com>
2025-05-07 10:17:43 +0800

    Add files via upload

---
+++ Assets/PostProcessing/LightAdjust.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+[ExecuteInEditMode]
+public class LightAdjust : MonoBehaviour
+{
+    private Volume volume;
+    [SerializeField, Range(0, 1)] private float setBright;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GetComponent<Volume>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (volume.profile.TryGet(out LiftGammaGain liftGammaGain))
+        {
+            liftGammaGain.gain.value = new Vector4(1, 1, 1, GlobalSettings.bright * 0.5f - 0.5f);
+        }
+    }
+}
